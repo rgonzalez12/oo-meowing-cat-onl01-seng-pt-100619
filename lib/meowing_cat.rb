bba75b980cbd5a2482af538406e932034aec25b3
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-24 04:54:32 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,11 +1,11 @@
 ## code your solution here. 
 class Cat 
   attr_accessor :cat
-  def name=(name)
-    @name = name
-  end
+  #def name=(name)
+    #@name = name
+  #end
   
-  def name
+  #def name
     @name
   end
   
